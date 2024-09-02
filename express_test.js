6236c9e574efda4ad3123d2beb2897919c7a5e58
Jake <jakereardon13@gmail.com>
2024-09-02 23:20:20 +0000

    added charms endpoint

--- express_test.js
+++ express_test.js
@@ -1,11 +1,19 @@
+const { MongoClient} = require("mongodb");
+require("dotenv").config()
 const express = require("express");
 const nodemailer = require("nodemailer");
 
 const app = express();
-const port = 8000;
+const port = 8080;
+
+const client = new MongoClient(process.env.MONGODB_URI);
+const charmsCollection = client.db("test").collection("charms");
 
 app.use(express.json());
 
+app.get("/api/charms", async (req, res) => {
+  res.json(await charmsCollection.find().toArray());
+});
 
 const transporter = nodemailer.createTransport({
   host: "smtp.zoho.com",
@@ -27,7 +35,7 @@ function formatJsonForMail(formJson) {
   return formattedInfo;
 }
 
-app.post("/", (req, res) => {
+app.post("/api/book-form", (req, res) => {
   const formDataJson = req.body;
 
   res.sendStatus(201);
