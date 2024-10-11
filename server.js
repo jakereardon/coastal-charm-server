565415b1f71990c67f11c41dee8474d7bae64454
Jake Reardon <jakereardon13@gmail.com>
2024-10-11 11:17:39 -0400

    added server item image functionality (in progress)

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
-require("dotenv").config()
+require("dotenv").config();
+const { createCanvas, loadImage } = require('canvas');
+const fs = require("fs");
 
 const { MongoClient} = require("mongodb");
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
@@ -22,10 +24,37 @@ function computeCartCost(cart) {
   return cart.reduce((totalCost, item) => totalCost + computeItemCost(item), 0);
 }
 
+function generateItemImage(item) {
+  const canvasWidth = 1000;
+  const canvasHeight = 1000;
+
+  const canvas = createCanvas(canvasWidth, canvasHeight);
+  const ctx = canvas.getContext('2d')
+
+  return loadImage("https://cc-nh.nyc3.digitaloceanspaces.com/chains/necklace-one.png")
+    .then(function(img) {
+      ctx.drawImage(img, 0, 0, canvasWidth, canvasHeight - 100);
+    })
+    .then(function() {
+      return Promise.all(item.charms.map(function(charm) {
+        return loadImage(charm.iconUrl)
+      }));
+    })
+    .then(function(imgPromises) {
+      imgPromises.forEach((img, i) => ctx.drawImage(img, item.charms[i].x, item.charms[i].y, 100, 100));
+    })
+    .then(() => {
+      return canvas;
+    })
+    .catch(err => console.log(err));
+}
+
 app.post("/create-confirm-intent", async (req, res) => {
-  const totalCost = computeCartCost(req.body.items);
+  const items = req.body.items;
+  const totalCost = computeCartCost(items);
 
   try {
+    // create and confirm Stripe payment intent
     const intent = await stripe.paymentIntents.create({
       confirm: true,
       amount: totalCost * 100,
@@ -33,10 +62,33 @@ app.post("/create-confirm-intent", async (req, res) => {
       confirmation_token: req.body.confirmationTokenId,
       return_url: "http://localhost:3000"
     });
+
+    
+    const intentId = intent.id;
+    
+    generateItemImage(items[0])
+      .then(function(img) {
+        const mailOptions = {
+          from: "noreply@coastalcharmnh.com",
+          to: "jakereardon13@gmail.com",
+          subject: "New Order #" + req.body.id,
+          html: "",
+          attachments: [{
+            filename: "test_image.png",
+            content: img.createPNGStream()
+          }]
+        }
+      });
+
     res.json({
       client_secret: intent.client_secret,
       status: intent.status
     });
+  
+    /*transporter.sendMail(mailOptions, (err, info) => {
+      if (err) console.log(err);
+    });*/
+
   } catch (err) {
     res.json({
       error: err
